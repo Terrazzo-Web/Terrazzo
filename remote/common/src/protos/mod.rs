9f1ddc1e3449a1ef732b4f0a3a46a8041a4270bc
Lamparo <LamparoS@Pavy.one>
2025-02-20 21:25:34 +0100

    test protos

--- remote/common/src/protos/mod.rs
+++ remote/common/src/protos/mod.rs
@@ -3,5 +3,10 @@ pub mod terrazzo {
         pub mod health {
             include!(concat!(env!("OUT_DIR"), "/terrazzo.remote.health.rs"));
         }
+
+        #[cfg(debug_assertions)]
+        pub mod tests {
+            include!(concat!(env!("OUT_DIR"), "/terrazzo.remote.tests.rs"));
+        }
     }
 }

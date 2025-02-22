397518d834f81d6daca1fdb2c246d956ffcca32d
Lamparo <LamparoS@Pavy.one>
2025-02-22 20:12:56 +0100

    More client tests

--- remote/client/src/tests.rs
+++ remote/client/src/tests.rs
@@ -1,37 +1,24 @@
 #![cfg(test)]
 
-use std::future::Future;
-use std::sync::Arc;
+use std::time::Duration;
 
-use test_client_config::TestClientConfig;
-use test_gateway_config::TestGatewayConfig;
-use test_tunnel_config::TestTunnelConfig;
-use tracing::info;
-use trz_gateway_common::certificate_info::CertificateInfo;
-use trz_gateway_common::id::ClientId;
 use trz_gateway_common::protos::terrazzo::remote::tests::Expression;
 use trz_gateway_common::protos::terrazzo::remote::tests::Value;
 use trz_gateway_common::protos::terrazzo::remote::tests::test_tunnel_service_client::TestTunnelServiceClient;
-use trz_gateway_server::auth_code::AuthCode;
-use trz_gateway_server::server::Server;
 
-use self::test_gateway_config::use_temp_dir;
-use crate::client::Client;
-use crate::client::connect::TunnelError;
-use crate::load_client_certificate::load_client_certificate;
+use self::end_to_end::EndToEnd;
 
+mod end_to_end;
 mod test_client_config;
 mod test_gateway_config;
 mod test_tunnel_config;
 
-const CLIENT_CERT_FILENAME: CertificateInfo<&str> = CertificateInfo {
-    certificate: "client-cert.pem",
-    private_key: "client-key.pem",
-};
-
 #[tokio::test]
 async fn end_to_end() -> Result<(), Box<dyn std::error::Error>> {
-    EndToEnd::run(|EndToEnd { client_id, server }| async move {
+    EndToEnd::run(|end_to_end| async move {
+        tokio::time::sleep(Duration::from_millis(1)).await;
+        let server = end_to_end.server;
+        let client_id = end_to_end.client_id;
         let channel = server
             .connections()
             .get_client(&client_id)
@@ -47,49 +34,4 @@ async fn end_to_end() -> Result<(), Box<dyn std::error::Error>> {
         Ok(())
     })
     .await
-}
-
-struct EndToEnd {
-    client_id: ClientId,
-    server: Arc<Server>,
-}
-
-impl EndToEnd {
-    async fn run<F: Future<Output = Result<(), Box<dyn std::error::Error>>>>(
-        test: impl FnOnce(Self) -> F,
-    ) -> Result<(), Box<dyn std::error::Error>> {
-        let temp_dir = use_temp_dir();
-
-        let gateway_config = TestGatewayConfig::new();
-        let (server, server_handle) = Server::run(gateway_config.clone()).await?;
-        info!("Started the server");
-
-        let client_id = ClientId::from("EndToEndClient");
-        let client_config = Arc::new(TestClientConfig::new(
-            gateway_config.clone(),
-            client_id.clone(),
-        ));
-
-        let auth_code = AuthCode::current().to_string();
-        let client_certificate = load_client_certificate(
-            client_config.clone(),
-            auth_code.into(),
-            CLIENT_CERT_FILENAME.map(|filename| temp_dir.path().join(filename)),
-        )
-        .await?
-        .into();
-        info!("Got the client certificate");
-
-        let tunnel_config = TestTunnelConfig::new(client_config.clone(), client_certificate);
-        let client = Client::new(tunnel_config).await?;
-        let client_handle = client.run().await?;
-        info!("The client is running");
-
-        let () = test(Self { client_id, server }).await?;
-
-        let () = server_handle.stop("End of test").await?;
-        let client_disconnected = client_handle.stopped().await?.unwrap_err();
-        assert!(matches!(client_disconnected, TunnelError::Disconnected));
-        Ok(())
-    }
 }
---
+++ remote/client/src/tests/end_to_end.rs
@@ -0,0 +1,84 @@
+use std::future::Future;
+use std::sync::Arc;
+
+use tracing::info;
+use trz_gateway_common::certificate_info::CertificateInfo;
+use trz_gateway_common::handle::ServerHandle;
+use trz_gateway_common::id::ClientId;
+use trz_gateway_common::security_configuration::certificate::pem::PemCertificate;
+use trz_gateway_server::auth_code::AuthCode;
+use trz_gateway_server::server::Server;
+
+use super::test_client_config::TestClientConfig;
+use super::test_gateway_config::TestGatewayConfig;
+use super::test_gateway_config::use_temp_dir;
+use super::test_tunnel_config::TestTunnelConfig;
+use crate::client::Client;
+use crate::client::connect::TunnelError;
+use crate::load_client_certificate::load_client_certificate;
+
+const CLIENT_CERT_FILENAME: CertificateInfo<&str> = CertificateInfo {
+    certificate: "client-cert.pem",
+    private_key: "client-key.pem",
+};
+
+pub struct EndToEnd<'t> {
+    pub client_id: ClientId,
+    #[expect(unused)]
+    pub client_certificate: Arc<PemCertificate>,
+    pub server: Arc<Server>,
+    #[expect(unused)]
+    pub client_handle: Box<dyn FnOnce() -> ServerHandle<Result<(), TunnelError>> + 't>,
+}
+
+impl<'t> EndToEnd<'t> {
+    pub async fn run<F: Future<Output = Result<(), Box<dyn std::error::Error>>>>(
+        test: impl FnOnce(EndToEnd) -> F + Send,
+    ) -> Result<(), Box<dyn std::error::Error>> {
+        let temp_dir = use_temp_dir();
+
+        let gateway_config = TestGatewayConfig::new();
+        let (server, server_handle) = Server::run(gateway_config.clone()).await?;
+        info!("Started the server");
+
+        let client_id = ClientId::from("EndToEndClient");
+        let client_config = Arc::new(TestClientConfig::new(
+            gateway_config.clone(),
+            client_id.clone(),
+        ));
+
+        let auth_code = AuthCode::current().to_string();
+        let client_certificate = Arc::new(
+            load_client_certificate(
+                client_config.clone(),
+                auth_code.into(),
+                CLIENT_CERT_FILENAME.map(|filename| temp_dir.path().join(filename)),
+            )
+            .await?,
+        );
+        info!("Got the client certificate");
+
+        let tunnel_config =
+            TestTunnelConfig::new(client_config.clone(), client_certificate.clone());
+        let client = Client::new(tunnel_config).await?;
+        let client_handle = client.run().await?;
+        info!("The client is running");
+
+        let mut client_handle = Some(client_handle);
+
+        let () = test(EndToEnd {
+            client_id,
+            server,
+            client_certificate,
+            client_handle: Box::new(|| client_handle.take().unwrap()),
+        })
+        .await?;
+
+        let () = server_handle.stop("End of test").await?;
+        if let Some(client_handle) = client_handle.take() {
+            let client_disconnected = client_handle.stopped().await?.unwrap_err();
+            assert!(matches!(client_disconnected, TunnelError::Disconnected));
+        }
+        Ok(())
+    }
+}

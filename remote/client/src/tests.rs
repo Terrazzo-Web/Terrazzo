802d7343ce2778213fa7992b36e0d452e1230811
Lamparo <LamparoS@Pavy.one>
2025-02-22 16:50:53 +0100

    .

--- remote/client/src/tests.rs
+++ remote/client/src/tests.rs
@@ -1,5 +1,6 @@
 #![cfg(test)]
 
+use std::future::Future;
 use std::sync::Arc;
 
 use test_client_config::TestClientConfig;
@@ -11,6 +12,7 @@ use trz_gateway_common::id::ClientId;
 use trz_gateway_common::protos::terrazzo::remote::tests::test_tunnel_service_client::TestTunnelServiceClient;
 use trz_gateway_common::protos::terrazzo::remote::tests::Expression;
 use trz_gateway_common::protos::terrazzo::remote::tests::Value;
+use trz_gateway_server::auth_code::AuthCode;
 use trz_gateway_server::server::Server;
 
 use self::test_gateway_config::use_temp_dir;
@@ -29,45 +31,65 @@ const CLIENT_CERT_FILENAME: CertificateInfo<&str> = CertificateInfo {
 
 #[tokio::test]
 async fn end_to_end() -> Result<(), Box<dyn std::error::Error>> {
-    let temp_dir = use_temp_dir();
+    EndToEnd::run(|EndToEnd { client_id, server }| async move {
+        let channel = server
+            .connections()
+            .get_client(&client_id)
+            .ok_or(format!("Client {client_id} not found"))?;
+        let mut grpc_client = TestTunnelServiceClient::new(channel);
+        let response = grpc_client
+            .calculate(tonic::Request::new(
+                { Expression::from(5) + Expression::from(2) * 3.into() }.into(),
+            ))
+            .await?
+            .into_inner();
+        assert_eq!(Value::from(11), response);
+        Ok(())
+    })
+    .await
+}
 
-    let gateway_config = TestGatewayConfig::new();
-    let (server, server_handle) = Server::run(gateway_config.clone()).await?;
-    info!("Started the server");
+struct EndToEnd {
+    client_id: ClientId,
+    server: Arc<Server>,
+}
 
-    let client_id = ClientId::from("EndToEndClient");
-    let client_config = Arc::new(TestClientConfig::new(
-        gateway_config.clone(),
-        client_id.clone(),
-    ));
+impl EndToEnd {
+    async fn run<F: Future<Output = Result<(), Box<dyn std::error::Error>>>>(
+        test: impl FnOnce(Self) -> F,
+    ) -> Result<(), Box<dyn std::error::Error>> {
+        let temp_dir = use_temp_dir();
 
-    let auth_code = trz_gateway_server::auth_code::AuthCode::current().to_string();
-    let client_certificate = load_client_certificate(
-        client_config.clone(),
-        auth_code.into(),
-        CLIENT_CERT_FILENAME.map(|filename| temp_dir.path().join(filename)),
-    )
-    .await?
-    .into();
-    info!("Got the client certificate");
+        let gateway_config = TestGatewayConfig::new();
+        let (server, server_handle) = Server::run(gateway_config.clone()).await?;
+        info!("Started the server");
 
-    let tunnel_config = TestTunnelConfig::new(client_config.clone(), client_certificate);
-    let client = Client::new(tunnel_config).await?;
-    let client_handle = client.run().await?;
-    info!("The client is running");
+        let client_id = ClientId::from("EndToEndClient");
+        let client_config = Arc::new(TestClientConfig::new(
+            gateway_config.clone(),
+            client_id.clone(),
+        ));
 
-    let channel = server.connections().get_client(&client_id);
-    let mut grpc_client = TestTunnelServiceClient::new(channel.unwrap());
-    let response = grpc_client
-        .calculate(tonic::Request::new(
-            (Expression::from(2) * Expression::from(3)).into(),
-        ))
+        let auth_code = AuthCode::current().to_string();
+        let client_certificate = load_client_certificate(
+            client_config.clone(),
+            auth_code.into(),
+            CLIENT_CERT_FILENAME.map(|filename| temp_dir.path().join(filename)),
+        )
         .await?
-        .into_inner();
-    assert_eq!(Value::from(6), response);
+        .into();
+        info!("Got the client certificate");
 
-    let () = server_handle.stop("End of test").await?;
-    let client_disconnected = client_handle.stopped().await?.unwrap_err();
-    assert!(matches!(client_disconnected, TunnelError::Disconnected));
-    Ok(())
+        let tunnel_config = TestTunnelConfig::new(client_config.clone(), client_certificate);
+        let client = Client::new(tunnel_config).await?;
+        let client_handle = client.run().await?;
+        info!("The client is running");
+
+        let () = test(Self { client_id, server }).await?;
+
+        let () = server_handle.stop("End of test").await?;
+        let client_disconnected = client_handle.stopped().await?.unwrap_err();
+        assert!(matches!(client_disconnected, TunnelError::Disconnected));
+        Ok(())
+    }
 }

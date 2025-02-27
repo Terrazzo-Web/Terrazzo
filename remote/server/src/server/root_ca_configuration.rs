41ed2e8bc207e6c4a26dbf643b673a81b777d269
Lamparo <LamparoS@Pavy.one>
2025-02-27 21:59:36 +0100

    SetPrivateKeyFilePermissions

--- remote/server/src/server/root_ca_configuration.rs
+++ remote/server/src/server/root_ca_configuration.rs
@@ -49,6 +49,16 @@ pub fn load_root_ca(
                 .zip(root_ca_pem.as_ref())
                 .try_map(|(path, pem): (&Path, &str)| std::fs::write(path, pem))
                 .map_err(RootCaConfigError::Store)?;
+
+            #[cfg(unix)]
+            {
+                use std::fs::Permissions;
+                use std::os::unix::fs::PermissionsExt as _;
+                let permissions = Permissions::from_mode(0o644);
+                std::fs::set_permissions(root_ca_path.private_key, permissions)
+                    .map_err(RootCaConfigError::SetPrivateKeyFilePermissions)?;
+            }
+
             Ok(root_ca_pem.into())
         }
         CertificateInfo {
@@ -83,4 +93,8 @@ pub enum RootCaConfigError {
 
     #[error("[{n}] {0}", n = self.name())]
     PemString(#[from] CertificateError<PemAsStringError>),
+
+    #[cfg(unix)]
+    #[error("[{n}] {0}", n = self.name())]
+    SetPrivateKeyFilePermissions(std::io::Error),
 }

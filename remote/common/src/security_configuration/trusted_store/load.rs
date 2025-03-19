bd08f2401208fa7c63c914c0b3f150e2cfc15e93
Lamparo <LamparoS@Pavy.one>
2025-03-19 21:46:38 +0100

    LoadTrustedStore

---
+++ remote/common/src/security_configuration/trusted_store/load.rs
@@ -0,0 +1,36 @@
+use nameth::NamedEnumValues as _;
+use nameth::nameth;
+
+use crate::unwrap_infallible::UnwrapInfallible as _;
+
+use super::cache::CachedTrustedStoreConfig;
+use super::native::NativeTrustedStoreConfig;
+use super::pem::PemTrustedStore;
+use super::pem::PemTrustedStoreError;
+
+pub enum LoadTrustedStore {
+    Native,
+    PEM(String),
+}
+
+impl LoadTrustedStore {
+    pub fn load(&self) -> Result<CachedTrustedStoreConfig, LoadTrustedStoreError> {
+        match self {
+            LoadTrustedStore::Native => {
+                Ok(CachedTrustedStoreConfig::new(NativeTrustedStoreConfig).unwrap_infallible())
+            }
+            LoadTrustedStore::PEM(root_certificates_pem) => {
+                Ok(CachedTrustedStoreConfig::new(PemTrustedStore {
+                    root_certificates_pem: root_certificates_pem.to_owned(),
+                })?)
+            }
+        }
+    }
+}
+
+#[nameth]
+#[derive(thiserror::Error, Debug)]
+pub enum LoadTrustedStoreError {
+    #[error("[{n}] {0}", n = self.name())]
+    LoadPem(#[from] PemTrustedStoreError),
+}

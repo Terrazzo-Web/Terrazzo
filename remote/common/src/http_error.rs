abf270b9ef3cddb30e94b8983da6fb6f295131e9
Lamparo <LamparoS@Pavy.one>
2025-03-25 21:20:17 +0100

    IsHttpError default impl

--- remote/common/src/http_error.rs
+++ remote/common/src/http_error.rs
@@ -1,3 +1,5 @@
+use std::convert::Infallible;
+
 use axum::http::StatusCode;
 use axum::response::IntoResponse;
 use axum::response::Response;
@@ -15,5 +17,35 @@ pub struct HttpError<E>(#[from] E);
 impl<E: IsHttpError> IntoResponse for HttpError<E> {
     fn into_response(self) -> Response {
         (self.0.status_code(), self.to_string()).into_response()
+    }
+}
+
+impl IsHttpError for Infallible {
+    fn status_code(&self) -> StatusCode {
+        unreachable!()
+    }
+}
+
+impl IsHttpError for tonic::Status {
+    fn status_code(&self) -> StatusCode {
+        match self.code() {
+            tonic::Code::Ok => StatusCode::OK,
+            tonic::Code::Cancelled => StatusCode::from_u16(499).expect("Client Closed Request"),
+            tonic::Code::Unknown => StatusCode::INTERNAL_SERVER_ERROR,
+            tonic::Code::InvalidArgument => StatusCode::BAD_REQUEST,
+            tonic::Code::DeadlineExceeded => StatusCode::GATEWAY_TIMEOUT,
+            tonic::Code::NotFound => StatusCode::NOT_FOUND,
+            tonic::Code::AlreadyExists => StatusCode::CONFLICT,
+            tonic::Code::PermissionDenied => StatusCode::FORBIDDEN,
+            tonic::Code::ResourceExhausted => StatusCode::TOO_MANY_REQUESTS,
+            tonic::Code::FailedPrecondition => StatusCode::BAD_REQUEST,
+            tonic::Code::Aborted => StatusCode::CONFLICT,
+            tonic::Code::OutOfRange => StatusCode::BAD_REQUEST,
+            tonic::Code::Unimplemented => StatusCode::NOT_IMPLEMENTED,
+            tonic::Code::Internal => StatusCode::INTERNAL_SERVER_ERROR,
+            tonic::Code::Unavailable => StatusCode::SERVICE_UNAVAILABLE,
+            tonic::Code::DataLoss => StatusCode::INTERNAL_SERVER_ERROR,
+            tonic::Code::Unauthenticated => StatusCode::UNAUTHORIZED,
+        }
     }
 }

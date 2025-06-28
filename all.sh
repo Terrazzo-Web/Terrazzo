85e7c3bc9235a9b07e81a4655ce1c375e2b5a7f6
Lamparo <LamparoS@Pavy.one>
2025-06-28 10:21:17 +0200

    Load assets from disk in debug mode

--- all.sh
+++ all.sh
@@ -7,9 +7,10 @@ cargo build --features=server
 cargo test --features=client
 cargo test --features=server
 cargo clippy --features=client,server
-cargo clippy --features client
-cargo clippy --features client,diagnostics
-cargo clippy --features server
+cargo clippy --features client,debug,diagnostics
+cargo clippy --features client --release
+cargo clippy --features server,debug,diagnostics
+cargo clippy --features server --release
 cargo doc --all-features
-cargo build --bin demo-server --features server,max_level_debug
-cargo build --bin demo-server --features server,diagnostics
+cargo build --bin demo-server --features server,max_level_debug,debug,diagnostics
+cargo build --bin demo-server --features server

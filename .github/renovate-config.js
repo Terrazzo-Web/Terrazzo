72c8dfde111958e321f8ee00e11b4adb8bc079bb
Lamparo <LamparoS@Pavy.one>
2025-11-22 19:06:36 +0100

    packagePatterns

--- .github/renovate-config.js
+++ .github/renovate-config.js
@@ -18,5 +18,9 @@ module.exports = {
                 'lockFileMaintenance',
             ],
         },
+        {
+            packagePatterns: ["^.*$"],
+            automerge: true
+        }
     ],
 };

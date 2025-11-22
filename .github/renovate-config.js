77b4ca3101c1d11caa0948940cf4b9a139916b03
Lamparo <LamparoS@Pavy.one>
2025-11-22 17:23:57 +0100

    Renovate (#94)

---
+++ .github/renovate-config.js
@@ -0,0 +1,22 @@
+module.exports = {
+    autoApprove: true,
+    automerge: true,
+    automergeStrategy: 'squash',
+    gitAuthor: 'Renovate Bot <bot@renovateapp.com>',
+    onboarding: false,
+    platform: 'github',
+    repositories: ['Terrazzo-Web/Terrazzo'],
+    packageRules: [
+        {
+            description: 'lockFileMaintenance',
+            matchUpdateTypes: [
+                'pin',
+                'digest',
+                'patch',
+                'minor',
+                'major',
+                'lockFileMaintenance',
+            ],
+        },
+    ],
+};

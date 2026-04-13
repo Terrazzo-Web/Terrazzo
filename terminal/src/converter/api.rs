2e3018839567ca2d8009fc833f5b3d129282c3ce
Lamparo <LamparoS@Pavy.one>
2026-04-13 19:15:07 +0200

    terrazzo_terminal_debug

--- terminal/src/converter/api.rs
+++ terminal/src/converter/api.rs
@@ -7,8 +7,7 @@ use terrazzo::server;
 
 use crate::api::client_address::ClientAddress;
 
-#[cfg_attr(not(debug_assertions), server(protocol = Http<Json, Json>))]
-#[cfg_attr(debug_assertions, server(protocol = Http<Json, Json>, endpoint = "get_conversions_for_tests"))]
+#[server(protocol = Http<Json, Json>)]
 pub async fn get_conversions(
     remote: Option<ClientAddress>,
     input: Arc<str>,
--- terminal/src/server.rs
+++ terminal/src/server.rs
@@ -6,7 +6,12 @@ fn main() {
     #[cfg(target_arch = "wasm32")]
     compile_error!();
 
-    terrazzo_terminal::run_server().unwrap_or_else(|error| {
+    #[cfg(all(feature = "bazel", feature = "debug"))]
+    let run_server = terrazzo_terminal_debug::run_server();
+    #[cfg(not(all(feature = "bazel", feature = "debug")))]
+    let run_server = terrazzo_terminal::run_server();
+
+    run_server.unwrap_or_else(|error| {
         if tracing::enabled!(Level::ERROR) {
             tracing::error!("{error}")
         } else {

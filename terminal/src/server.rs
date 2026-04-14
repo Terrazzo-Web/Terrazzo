c7812721aff84f7efd786e36c530893814b6d9f6
Lamparo <LamparoS@Pavy.one>
2026-04-14 15:39:36 +0200

    aliases

--- terminal/src/server.rs
+++ terminal/src/server.rs
@@ -1,3 +1,4 @@
+// TODO remove this!!!
 #![allow(unused_crate_dependencies)]
 
 use tracing::Level;
@@ -6,12 +7,7 @@ fn main() {
     #[cfg(target_arch = "wasm32")]
     compile_error!();
 
-    #[cfg(all(feature = "bazel", feature = "debug"))]
-    let run_server = terrazzo_terminal_debug::run_server();
-    #[cfg(not(all(feature = "bazel", feature = "debug")))]
-    let run_server = terrazzo_terminal::run_server();
-
-    run_server.unwrap_or_else(|error| {
+    terrazzo_terminal::run_server().unwrap_or_else(|error| {
         if tracing::enabled!(Level::ERROR) {
             tracing::error!("{error}")
         } else {

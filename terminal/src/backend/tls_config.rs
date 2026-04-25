c11f4ec59f1a6bebec83ddfbb3ab27a89dfee560
Lamparo <LamparoS@Pavy.one>
2026-04-25 12:40:08 +0200

    wip tls_config

--- terminal/src/backend/tls_config.rs
+++ terminal/src/backend/tls_config.rs
@@ -1,3 +1,6 @@
+use std::time::Duration;
+use std::time::SystemTime;
+
 use nameth::NamedEnumValues as _;
 use nameth::nameth;
 use trz_gateway_common::security_configuration::SecurityConfig;
@@ -8,14 +11,13 @@ use trz_gateway_common::security_configuration::certificate::pem::PemCertificate
 use trz_gateway_common::unwrap_infallible::UnwrapInfallible as _;
 use trz_gateway_common::x509::PemAsStringError;
 use trz_gateway_common::x509::PemString as _;
-use trz_gateway_common::x509::ca::MakeCaError;
-use trz_gateway_common::x509::ca::make_intermediate;
 use trz_gateway_common::x509::cert::MakeCertError;
 use trz_gateway_common::x509::cert::make_cert;
 use trz_gateway_common::x509::key::MakeKeyError;
 use trz_gateway_common::x509::key::make_key;
 use trz_gateway_common::x509::name::CertitficateName;
 use trz_gateway_common::x509::time::Asn1ToSystemTimeError;
+use trz_gateway_common::x509::validity::Validity;
 use trz_gateway_common::x509::validity::ValidityError;
 
 use super::root_ca_config::PrivateRootCa;
@@ -26,19 +28,9 @@ pub fn make_tls_config(
     let root_ca_x509 = root_ca.certificate().unwrap_infallible();
     let validity = root_ca_x509.certificate.as_ref().try_into()?;
 
-    let intermediate = make_intermediate(
-        (*root_ca_x509).as_ref(),
-        CertitficateName {
-            organization: Some("Terrazzo"),
-            common_name: Some("Terrazzo Terminal Intermediate CA"),
-            ..CertitficateName::default()
-        },
-        validity,
-    )?;
-
     let certificate_key = make_key()?;
     let certificate = make_cert(
-        intermediate.as_ref(),
+        (*root_ca_x509).as_ref(),
         CertitficateName {
             organization: Some("Terrazzo"),
             common_name: Some("localhost"),
@@ -52,13 +44,12 @@ pub fn make_tls_config(
         vec![],
     )?;
 
-    let intermediates_pem = intermediate.certificate.to_pem().pem_string();
     let certificate_pem = certificate.to_pem().pem_string();
     let private_key_pem = certificate_key.private_key_to_pem_pkcs8().pem_string();
     Ok(SecurityConfig {
         trusted_store: root_ca.clone(),
         certificate: PemCertificate {
-            intermediates_pem: intermediates_pem.map_err(TlsConfigError::IntermediatesPem)?,
+            intermediates_pem: String::new(),
             certificate_pem: certificate_pem.map_err(TlsConfigError::CertificatePem)?,
             private_key_pem: private_key_pem.map_err(TlsConfigError::PrivateKeyPem)?,
         }
@@ -73,16 +64,10 @@ pub enum TlsConfigError {
     ValidityError(#[from] ValidityError<Asn1ToSystemTimeError>),
 
     #[error("[{n}] {0}", n = self.name())]
-    MakeIntermediate(#[from] MakeCaError),
-
-    #[error("[{n}] {0}", n = self.name())]
     MakeKey(#[from] MakeKeyError),
 
     #[error("[{n}] {0}", n = self.name())]
     MakeCertificate(#[from] MakeCertError),
-
-    #[error("[{n}] {0}", n = self.name())]
-    IntermediatesPem(PemAsStringError),
 
     #[error("[{n}] {0}", n = self.name())]
     CertificatePem(PemAsStringError),

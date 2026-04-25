3df85b31ed146ac87bf40842c739ee84ccf8faf7
Lamparo <LamparoS@Pavy.one>
2026-04-25 12:40:08 +0200

    Bypass intermediate for macos v2

--- terminal/src/backend/tls_config.rs
+++ terminal/src/backend/tls_config.rs
@@ -1,5 +1,6 @@
 use nameth::NamedEnumValues as _;
 use nameth::nameth;
+use openssl::x509::X509;
 use trz_gateway_common::security_configuration::SecurityConfig;
 use trz_gateway_common::security_configuration::certificate::CertificateConfig as _;
 use trz_gateway_common::security_configuration::certificate::cache::CachedCertificate;
@@ -26,40 +27,68 @@ pub fn make_tls_config(
     let root_ca_x509 = root_ca.certificate().unwrap_infallible();
     let validity = root_ca_x509.certificate.as_ref().try_into()?;
 
-    let intermediate = make_intermediate(
-        (*root_ca_x509).as_ref(),
-        CertitficateName {
-            organization: Some("Terrazzo"),
-            common_name: Some("Terrazzo Terminal Intermediate CA"),
-            ..CertitficateName::default()
-        },
-        validity,
-    )?;
-
     let certificate_key = make_key()?;
-    let certificate = make_cert(
-        intermediate.as_ref(),
-        CertitficateName {
-            organization: Some("Terrazzo"),
-            common_name: Some("localhost"),
-            ..CertitficateName::default()
-        },
-        validity,
-        &certificate_key
-            .public_key_to_pem()
-            .pem_string()
-            .map_err(TlsConfigError::PublicKeyPem)?,
-        vec![],
-    )?;
-
-    let intermediates_pem = intermediate.certificate.to_pem().pem_string();
-    let certificate_pem = certificate.to_pem().pem_string();
     let private_key_pem = certificate_key.private_key_to_pem_pkcs8().pem_string();
+
+    let certificate: X509;
+    let intermediates_pem: String;
+
+    if cfg!(target_os = "macos") {
+        certificate = make_cert(
+            (*root_ca_x509).as_ref(),
+            CertitficateName {
+                organization: Some("Terrazzo"),
+                common_name: Some("localhost"),
+                ..CertitficateName::default()
+            },
+            validity,
+            &certificate_key
+                .public_key_to_pem()
+                .pem_string()
+                .map_err(TlsConfigError::PublicKeyPem)?,
+            vec![],
+        )?;
+
+        intermediates_pem = Default::default();
+    } else {
+        let intermediate = make_intermediate(
+            (*root_ca_x509).as_ref(),
+            CertitficateName {
+                organization: Some("Terrazzo"),
+                common_name: Some("Terrazzo Terminal Intermediate CA"),
+                ..CertitficateName::default()
+            },
+            validity,
+        )?;
+        certificate = make_cert(
+            intermediate.as_ref(),
+            CertitficateName {
+                organization: Some("Terrazzo"),
+                common_name: Some("localhost"),
+                ..CertitficateName::default()
+            },
+            validity,
+            &certificate_key
+                .public_key_to_pem()
+                .pem_string()
+                .map_err(TlsConfigError::PublicKeyPem)?,
+            vec![],
+        )?;
+        intermediates_pem = {
+            let pem = intermediate.certificate.to_pem();
+            pem.pem_string().map_err(TlsConfigError::IntermediatesPem)?
+        };
+    }
+
+    let certificate_pem = {
+        let pem = certificate.to_pem();
+        pem.pem_string().map_err(TlsConfigError::CertificatePem)?
+    };
     Ok(SecurityConfig {
         trusted_store: root_ca.clone(),
         certificate: PemCertificate {
-            intermediates_pem: intermediates_pem.map_err(TlsConfigError::IntermediatesPem)?,
-            certificate_pem: certificate_pem.map_err(TlsConfigError::CertificatePem)?,
+            intermediates_pem,
+            certificate_pem,
             private_key_pem: private_key_pem.map_err(TlsConfigError::PrivateKeyPem)?,
         }
         .cache()?,

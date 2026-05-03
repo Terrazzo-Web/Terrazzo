adb72aed96135d66d109a1a03e03c1b336be15bf
Lamparo <LamparoS@Pavy.one>
2026-05-03 22:14:08 +0200

    CLI

---
+++ utils/synctex/cli/src/main.rs
@@ -0,0 +1,16 @@
+use std::process::ExitCode;
+
+use clap as _;
+#[cfg(not(feature = "bazel"))]
+use terrazzo_synctex as _;
+use thiserror as _;
+
+fn main() -> ExitCode {
+    match terrazzo_synctex_cli::run(std::env::args_os(), std::io::stdout().lock()) {
+        Ok(()) => ExitCode::SUCCESS,
+        Err(error) => {
+            eprintln!("{error}");
+            ExitCode::FAILURE
+        }
+    }
+}

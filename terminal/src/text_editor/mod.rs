7e53300b5ff9606c9fa03c5948118802f1492614
Lamparo <LamparoS@Pavy.one>
2026-05-20 20:04:32 +0200

    TODO text editor

--- terminal/src/text_editor/mod.rs
+++ terminal/src/text_editor/mod.rs
@@ -15,3 +15,22 @@ pub mod ui;
 
 #[cfg(feature = "client")]
 terrazzo_css::import_style!(style, "text_editor.scss");
+
+/* TODO
+Open multiple app tiles
+- Review usage of global state
+- Each app has an address
+  - [] = root pane, current
+  - ['h']
+
+Side-by-side view of .tex and .pdf
+- The logic is, when opening a file, check if there are other files with the same extension. If yes, show a dropdown of such files. If selected, split the pane in two and show two editors
+
+Search engine
+- implement tantivy
+
+Folder view
+- not just a view of previously opened files
+
+Save cursor position
+ */

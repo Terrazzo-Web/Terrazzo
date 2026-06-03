b65dfda3dbe7c39e59843026355946f47632bf89
Lamparo <LamparoS@Pavy.one>
2026-06-03 20:59:31 +0200

    Revert "maybe"
    This reverts commit 9d41c80a4f7337d58991dcc9db74ad7ac21d6479.

--- terminal/src/text_editor/side/mod.rs
+++ terminal/src/text_editor/side/mod.rs
@@ -8,12 +8,11 @@ pub mod ui;
 
 #[derive(Clone, serde::Serialize, serde::Deserialize)]
 #[cfg_attr(feature = "server", allow(dead_code))]
-#[serde(bound(serialize = "R: opqaue::T", deserialize = "R: opqaue::T"))]
-pub struct SideViewNode<R: opqaue::T = opqaue::OpaqueNotifyRegistration> {
+pub struct SideViewNode {
     #[cfg_attr(not(feature = "diagnostics"), serde(rename = "p"))]
     pub properties: SvnProperties,
     #[cfg_attr(not(feature = "diagnostics"), serde(rename = "i"))]
-    pub item: SvnItem<R>,
+    pub item: SvnItem,
 }
 
 #[derive(Clone, Default, serde::Serialize, serde::Deserialize)]
@@ -35,15 +34,14 @@ pub enum SvnStatus {
 
 #[derive(Clone, serde::Serialize, serde::Deserialize)]
 #[cfg_attr(feature = "server", allow(dead_code))]
-#[serde(bound(serialize = "R: opqaue::T", deserialize = "R: opqaue::T"))]
-pub enum SvnItem<R: opqaue::T = opqaue::OpaqueNotifyRegistration> {
+pub enum SvnItem {
     #[cfg_attr(not(feature = "diagnostics"), serde(rename = "D"))]
-    Folder(Arc<SideViewList<R>>),
+    Folder(Arc<SideViewList>),
     #[cfg_attr(not(feature = "diagnostics"), serde(rename = "F"))]
     File {
         metadata: Arc<FileMetadata>,
         #[serde(skip)]
-        notify_registration: R,
+        notify_registration: opqaue::OpaqueNotifyRegistration,
     },
 }
 
@@ -51,13 +49,7 @@ pub mod opqaue {
     use std::any::Any;
     use std::rc::Rc;
 
-    use serde::de::DeserializeOwned;
-
-    pub trait T: Clone + Default + serde::Serialize + DeserializeOwned {}
-    impl<TT: Clone + Default + serde::Serialize + DeserializeOwned> T for TT {}
-
     #[derive(Clone, Default)]
-    #[allow(dead_code)]
     pub struct OpaqueNotifyRegistration(Option<Rc<dyn Any>>);
 
     #[cfg(feature = "client")]
@@ -73,29 +65,9 @@ pub mod opqaue {
         }
     }
 
-    #[allow(dead_code)]
     impl OpaqueNotifyRegistration {
         pub fn is_set(&self) -> bool {
             self.0.is_some()
-        }
-    }
-
-    impl serde::Serialize for OpaqueNotifyRegistration {
-        fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
-        where
-            S: serde::Serializer,
-        {
-            serializer.serialize_unit()
-        }
-    }
-
-    impl<'de> serde::Deserialize<'de> for OpaqueNotifyRegistration {
-        fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
-        where
-            D: serde::Deserializer<'de>,
-        {
-            <()>::deserialize(deserializer)?;
-            Ok(Self::default())
         }
     }
 
@@ -103,48 +75,19 @@ pub mod opqaue {
     unsafe impl Sync for OpaqueNotifyRegistration {}
 }
 
-#[derive(Clone, Default, serde::Serialize, serde::Deserialize)]
-#[cfg_attr(feature = "server", allow(dead_code))]
-#[serde(transparent)]
-#[serde(bound(serialize = "R: opqaue::T", deserialize = "R: opqaue::T"))]
-pub struct SideViewList<R: opqaue::T = opqaue::OpaqueNotifyRegistration>(
-    BTreeMap<Arc<str>, Arc<SideViewNode<R>>>,
-);
+pub type SideViewList = BTreeMap<Arc<str>, Arc<SideViewNode>>;
 
-impl<R: opqaue::T> std::fmt::Debug for SideViewList<R> {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        f.debug_tuple("SideViewList").field(&self.0).finish()
-    }
-}
-
-impl<R: opqaue::T> std::ops::Deref for SideViewList<R> {
-    type Target = BTreeMap<Arc<str>, Arc<SideViewNode<R>>>;
-
-    fn deref(&self) -> &Self::Target {
-        &self.0
-    }
-}
-
-impl<R: opqaue::T> std::ops::DerefMut for SideViewList<R> {
-    fn deref_mut(&mut self) -> &mut Self::Target {
-        &mut self.0
-    }
-}
-
-impl<R: opqaue::T> FromIterator<(Arc<str>, Arc<SideViewNode<R>>)> for SideViewList<R> {
-    fn from_iter<T: IntoIterator<Item = (Arc<str>, Arc<SideViewNode<R>>)>>(iter: T) -> Self {
-        Self(BTreeMap::from_iter(iter))
-    }
-}
-
-impl<R: opqaue::T> std::fmt::Debug for SideViewNode<R> {
+impl std::fmt::Debug for SideViewNode {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match &self.item {
             SvnItem::Folder(children) => f.debug_tuple("Folder").field(children).finish(),
             SvnItem::File {
                 metadata,
-                notify_registration: _,
-            } => f.debug_tuple("File").field(&metadata.name).finish(),
+                notify_registration,
+            } => {
+                let _ = notify_registration.is_set();
+                f.debug_tuple("File").field(&metadata.name).finish()
+            }
         }
     }
 }

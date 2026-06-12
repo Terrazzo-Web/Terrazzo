d5c138a8610046d48203ad84eb5e3152e60a34ca
Lamparo <LamparoS@Pavy.one>
2026-06-12 23:06:34 +0200

    tabs

--- terminal/src/tiles/tabs.rs
+++ terminal/src/tiles/tabs.rs
@@ -1,5 +1,6 @@
 use std::rc::Rc;
 
+use terrazzo::autoclone;
 use terrazzo::html;
 use terrazzo::prelude::*;
 use terrazzo::template;
@@ -11,6 +12,7 @@ use terrazzo::widgets::tabs::TabsState;
 use terrazzo::widgets::tabs::tabs;
 use wasm_bindgen_futures::spawn_local;
 
+use self::diagnostics::warn;
 use super::api::Direction;
 use super::signals::Tiles;
 use super::ui::RcSlice;
@@ -28,14 +30,12 @@ pub struct TileTabs {
 
 impl TileTabs {
     pub fn new(nodes: &[Rc<Tiles>], selected: &XSignal<Option<TileId>>) -> Self {
+        let tabs = nodes
+            .iter()
+            .map(|node| TileTab::new(node.clone(), selected))
+            .collect();
         Self {
-            tabs: Rc::new(
-                nodes
-                    .iter()
-                    .cloned()
-                    .map(|node| TileTab::new(node, selected))
-                    .collect(),
-            ),
+            tabs: Rc::new(tabs),
         }
     }
 }
@@ -48,26 +48,25 @@ impl TabsDescriptor for TileTabs {
         &self.tabs
     }
 
+    #[autoclone]
     #[html]
     fn after_titles(&self, state: &TileTabsState) -> impl IntoIterator<Item = impl Into<XNode>> {
-        let state = state.clone();
         [div(
             class = style::ADD_TILE_TAB,
             #[cfg(not(feature = "client-prod"))]
             class = "add-tile-tab",
             img(src = icons::add_tab()),
             click = move |_| {
-                let state = state.clone();
+                autoclone!(state);
                 spawn_local(async move {
+                    autoclone!(state);
                     let after_child = state.selected.get_value_untracked();
                     match super::api::add_tab(state.array_id, after_child).await {
                         Ok((tree, new_id)) => {
                             state.selected.set(Some(new_id));
                             RootTree::update(Ok::<_, String>(tree));
                         }
-                        Err(error) => {
-                            diagnostics::warn!("Failed to add tile tab: {error}");
-                        }
+                        Err(error) => warn!("Failed to add tile tab: {error}"),
                     }
                 });
             },
@@ -115,7 +114,7 @@ impl TabsState for TileTabsState {
             {
                 Some(moved_child) => moved_child,
                 None => {
-                    diagnostics::warn!("Invalid tile tab id: {moved_tab_key}");
+                    warn!("Invalid tile tab id: {moved_tab_key}");
                     return;
                 }
             };
@@ -207,46 +206,46 @@ pub fn show_tabbed_tiles(
             descriptor,
             state,
             Ptr::new(TabsOptions {
-                tabs_class: Some(get_class_name("tile-tabs", style::TILE_TABS).into()),
-                titles_class: Some(
-                    get_class_name("tile-tab-titles", style::TILE_TAB_TITLES).into(),
-                ),
-                title_class: Some(get_class_name("tile-tab-title", style::TILE_TAB_TITLE).into()),
-                items_class: Some(get_class_name("tile-tab-items", style::TILE_TAB_ITEMS).into()),
-                item_class: Some(get_class_name("tile-tab-item", style::TILE_TAB_ITEM).into()),
-                selected_class: Some(get_class_name("selected", style::SELECTED).into()),
+                tabs_class: Some(get_class_name("tile-tabs", style::TILE_TABS)),
+                titles_class: Some(get_class_name("tile-tab-titles", style::TILE_TAB_TITLES)),
+                title_class: Some(get_class_name("tile-tab-title", style::TILE_TAB_TITLE)),
+                items_class: Some(get_class_name("tile-tab-items", style::TILE_TAB_ITEMS)),
+                item_class: Some(get_class_name("tile-tab-item", style::TILE_TAB_ITEM)),
+                selected_class: Some(get_class_name("selected", style::SELECTED)),
                 ..TabsOptions::default()
             }),
         ),
     )
 }
 
-fn get_class_name(name: &'static str, class: &'static str) -> impl Into<XString> {
+fn get_class_name(name: &'static str, class: &'static str) -> XString {
     #[cfg(feature = "client-prod")]
     {
         let _ = name;
-        return class;
+        return class.into();
     }
 
     #[cfg(not(feature = "client-prod"))]
-    return format!("{name} {class}");
+    return format!("{name} {class}").into();
 }
 
+#[autoclone]
 #[html]
 fn tile_title(tile: super::signals::TilePtr, selected: XSignal<Option<TileId>>) -> XElement {
     let tile_id = tile.id;
-    let app = tile.app.clone();
-    let app2 = tile.app.clone();
-    let title = tile.title.clone().derive(
+    let title = tile.title.derive(
         "resolve-tile-title",
         move |title| {
-            title
-                .clone()
-                .unwrap_or_else(|| app.get_value_untracked().to_string().into())
+            autoclone!(tile);
+            if let Some(title) = title {
+                title.clone()
+            } else {
+                tile.app.get_value_untracked().to_string().into()
+            }
         },
         if_change(move |_, title: &XString| {
-            let default_title = app2.get_value_untracked().to_string();
-            if title.is_empty() || title.to_string() == default_title {
+            autoclone!(tile);
+            if title.is_empty() || title.to_string() == tile.app.get_value_untracked().to_string() {
                 Some(None)
             } else {
                 Some(Some(title.clone()))
@@ -258,23 +257,21 @@ fn tile_title(tile: super::signals::TilePtr, selected: XSignal<Option<TileId>>) 
         *selected == Some(tile_id)
     });
     span(move |template| {
-        let title = title.clone();
-        let title2 = title.clone();
         editable(
             template,
             title.clone(),
             is_editable.clone(),
             editing.clone(),
             move || {
-                let title = title2.clone();
-                [span(move |template| print_title(template, title.clone()))]
+                autoclone!(title);
+                [print_title(title.clone())]
             },
         )
     })
 }
 
 #[html]
-#[template]
+#[template(tag = span)]
 fn print_title(#[signal] title: XString) -> XElement {
     span("{title}")
 }

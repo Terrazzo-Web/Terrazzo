f4d2bc51e6c35acd6087e9a522aa1ada000438a9
Lamparo <LamparoS@Pavy.one>
2026-06-14 10:27:39 +0200

    tracing

--- terminal/src/backend/client_service/text_editor_service/callback.rs
+++ terminal/src/backend/client_service/text_editor_service/callback.rs
@@ -7,10 +7,14 @@ use futures::StreamExt as _;
 use futures::TryStreamExt as _;
 use futures::stream;
 use prost::bytes::Bytes;
+use scopeguard::defer;
 use tonic::body::Body as BoxBody;
 use tonic::client::GrpcService;
 use tonic::codegen::StdError;
 use tonic::transport::Body;
+use tracing::Instrument;
+use tracing::debug;
+use tracing::debug_span;
 
 use super::DownloadStream;
 use super::TextEditorFsioError;
@@ -50,6 +54,7 @@ impl DistributedCallback for DownloadCallback {
         _server: Option<&Arc<Server>>,
         path: Self::Request,
     ) -> Result<Self::Response, Self::LocalError> {
+        debug!("Downloading file {path:?}");
         download_local(path).await
     }
 
@@ -64,18 +69,40 @@ impl DistributedCallback for DownloadCallback {
         T::ResponseBody: Body<Data = tonic::codegen::Bytes> + Send + 'static,
         <T::ResponseBody as Body>::Error: Into<StdError> + Send,
     {
-        let mut client = TextEditorServiceClient::new(channel);
-        let response = client
-            .download(DownloadRequest {
-                address: Some(ClientAddressProto::of(client_address)),
-                path: Some(path.into()),
-            })
-            .await?
-            .into_inner()
-            .map_ok(|response| response.data)
-            .map_err(TextEditorFsioError::from);
-        Ok(Box::pin(response))
+        let address = ClientAddressProto::of(client_address);
+        let span = debug_span!("Downloading file", ?path, ?address);
+        async move {
+            debug!("Start");
+            defer!(debug!("Done"));
+            remote_download_impl(channel, address, path).await
+        }
+        .instrument(span)
+        .await
     }
+}
+
+async fn remote_download_impl<T>(
+    channel: T,
+    address: ClientAddressProto,
+    path: FilePath<std::path::PathBuf>,
+) -> Result<DownloadStream, tonic::Status>
+where
+    T: GrpcService<BoxBody>,
+    T::Error: Into<StdError>,
+    T::ResponseBody: Body<Data = tonic::codegen::Bytes> + Send + 'static,
+    <T::ResponseBody as Body>::Error: Into<StdError> + Send,
+{
+    let mut client = TextEditorServiceClient::new(channel);
+    let response = client
+        .download(DownloadRequest {
+            address: Some(address),
+            path: Some(path.into()),
+        })
+        .await?
+        .into_inner()
+        .map_ok(|response| response.data)
+        .map_err(TextEditorFsioError::from);
+    Ok(Box::pin(response))
 }
 
 pub struct UploadCallback<S>(PhantomData<S>);
@@ -109,6 +136,7 @@ where
         _server: Option<&Arc<Server>>,
         (path, content): Self::Request,
     ) -> Result<Self::Response, Self::LocalError> {
+        debug!("Uploading file {path:?}");
         upload_local(path, content).await
     }
 
@@ -123,25 +151,46 @@ where
         T::ResponseBody: Body<Data = tonic::codegen::Bytes> + Send + 'static,
         <T::ResponseBody as Body>::Error: Into<StdError> + Send,
     {
-        let first = UploadRequest {
-            kind: Some(upload_request::Kind::Address(ClientAddressProto::of(
-                client_address,
-            ))),
-        };
-        let second = UploadRequest {
-            kind: Some(upload_request::Kind::Path(path.into())),
-        };
-        let content = content.map_ok(|data| UploadRequest {
-            kind: Some(upload_request::Kind::Data(data)),
-        });
-        let content = stream::iter([Ok(first), Ok(second)])
-            .chain(content)
-            .inspect_err(super::warn_stream_error)
-            .filter_map(|request| ready(request.ok()));
-        let mut client = TextEditorServiceClient::new(channel);
-        client.upload(content).await?;
-        Ok(())
+        let address = ClientAddressProto::of(client_address);
+        let span = debug_span!("Downlaoding file", ?path, ?address);
+        async move {
+            debug!("Start");
+            defer!(debug!("Done"));
+            remote_upload_impl(channel, address, (path, content)).await
+        }
+        .instrument(span)
+        .await
     }
+}
+
+async fn remote_upload_impl<S, T>(
+    channel: T,
+    address: ClientAddressProto,
+    (path, content): (FilePath<std::path::PathBuf>, S),
+) -> Result<(), tonic::Status>
+where
+    S: Stream<Item = Result<Bytes, TextEditorFsioError>> + Send + 'static,
+    T: GrpcService<BoxBody>,
+    T::Error: Into<StdError>,
+    T::ResponseBody: Body<Data = tonic::codegen::Bytes> + Send + 'static,
+    <T::ResponseBody as Body>::Error: Into<StdError> + Send,
+{
+    let first = UploadRequest {
+        kind: Some(upload_request::Kind::Address(address)),
+    };
+    let second = UploadRequest {
+        kind: Some(upload_request::Kind::Path(path.into())),
+    };
+    let content = content.map_ok(|data| UploadRequest {
+        kind: Some(upload_request::Kind::Data(data)),
+    });
+    let content = stream::iter([Ok(first), Ok(second)])
+        .chain(content)
+        .inspect_err(super::warn_stream_error)
+        .filter_map(|request| ready(request.ok()));
+    let mut client = TextEditorServiceClient::new(channel);
+    client.upload(content).await?;
+    Ok(())
 }
 
 fn map_distributed_error(

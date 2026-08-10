4153d97fc34c25abe58e0025a2bbc2a36ed8e076
Lamparo <LamparoS@Pavy.one>
2026-08-10 22:35:08 +0200

    Toggle text editor search from icon

--- terminal/src/text_editor/search/ui.rs
+++ terminal/src/text_editor/search/ui.rs
@@ -42,6 +42,7 @@ impl TextEditorManager {
     pub fn search_selector(self: &Ptr<Self>) -> XElement {
         let is_active = self.search.is_active.clone();
         let input: ElementCapture<HtmlInputElement> = ElementCapture::default();
+        let manager = self.clone();
 
         return div(
             class = style::PATH_SELECTOR,
@@ -51,7 +52,11 @@ impl TextEditorManager {
                 class = style::SEARCH_ICON,
                 src = icons::search(),
                 click = move |_| {
-                    autoclone!(is_active, input);
+                    autoclone!(manager, is_active, input);
+                    if is_active.get_value_untracked() {
+                        close_search(&manager);
+                        return;
+                    }
                     is_active.set(true);
                     let () = input.with(|i| i.focus()).or_throw("focus");
                 },
@@ -73,7 +78,7 @@ fn search_selector_input(
     manager: Ptr<TextEditorManager>,
     input: ElementCapture<HtmlInputElement>,
     #[signal] base: Arc<Path>,
-    #[signal] mut is_active: bool,
+    #[signal] is_active: bool,
 ) -> XElement {
     if !is_active {
         return tag(style::display = "none", style::visibility = "hidden");
@@ -88,10 +93,10 @@ fn search_selector_input(
             r#type = "text",
             class = style::PATH_SELECTOR_FIELD,
             keydown = move |event: KeyboardEvent| {
-                autoclone!(manager, is_active_mut, input, do_search);
+                autoclone!(manager, input, do_search);
                 if event.key() == "Escape" {
                     event.prevent_default();
-                    close_search(&manager, &is_active_mut);
+                    close_search(&manager);
                     let () = input.with(|i| i.blur()).or_throw("blur");
                     return;
                 }
@@ -132,7 +137,7 @@ fn start_search(manager: &Ptr<TextEditorManager>, do_search: &Ptr<impl Fn()>) {
     do_search()
 }
 
-fn close_search(manager: &TextEditorManager, is_active_mut: &MutableSignal<bool>) {
+fn close_search(manager: &TextEditorManager) {
     manager.editor_state.update(|editor_state| {
         let EditorState::Search(EditorSearchState { prev, .. }) = editor_state else {
             return None;
@@ -143,7 +148,7 @@ fn close_search(manager: &TextEditorManager, is_active_mut: &MutableSignal<bool>
         manager.side_view.force(prev_side_view);
         manager.search.prev_side_view.force(None);
     }
-    is_active_mut.set(false);
+    manager.search.is_active.set(false);
 }
 
 fn do_search(

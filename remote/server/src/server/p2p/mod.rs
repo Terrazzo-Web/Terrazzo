a31c5c7ff6189b64d39d42c3ee2880c617e967a7
Lamparo <LamparoS@Pavy.one>
2026-08-30 12:39:04 +0200

    Wip task 2

---
+++ remote/server/src/server/p2p/mod.rs
@@ -0,0 +1,33 @@
+//! Public WebRTC signaling endpoints.
+
+use std::sync::Arc;
+
+use axum::extract::Path;
+use axum::extract::WebSocketUpgrade;
+use axum::response::Response;
+use trz_gateway_common::id::ClientName;
+
+use super::Server;
+
+pub(super) mod signaling;
+
+impl Server {
+    pub(super) async fn p2p_register(
+        self: Arc<Self>,
+        Path(server_name): Path<ClientName>,
+        web_socket: WebSocketUpgrade,
+    ) -> Response {
+        self.p2p_signaling.clone().register(server_name, web_socket)
+    }
+
+    pub(super) async fn p2p_connect(
+        self: Arc<Self>,
+        Path(server_name): Path<ClientName>,
+        web_socket: WebSocketUpgrade,
+    ) -> Response {
+        self.p2p_signaling
+            .clone()
+            .connect(server_name, web_socket)
+            .await
+    }
+}
